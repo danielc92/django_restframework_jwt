6510c1c389e34ec26b1984be599e70216d3a2d68
Daniel Corcoran <danielc.92@outlook.com>
2019-06-17 14:11:36 +1000

    Added view for programmer

--- languages/views.py
+++ languages/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .models import Language
-from .serializers import LanguageSerializer
+from .models import Language, Programmer
+from .serializers import LanguageSerializer, ProgrammerSerializer
 
 # Create your views here.
 class LanguageView(viewsets.ModelViewSet):
     
     queryset = Language.objects.all()
     serializer_class = LanguageSerializer
+
+class ProgrammerView(viewsets.ModelViewSet):
+    
+    queryset = Programmer.objects.all()
+    serializer_class = ProgrammerSerializer
