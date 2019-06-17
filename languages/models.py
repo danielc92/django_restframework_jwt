0437d61c64f9d1398f811e6583e3a4536bf46a95
Daniel Corcoran <danielc.92@outlook.com>
2019-06-17 13:35:24 +1000

    renamed a column

--- languages/models.py
+++ languages/models.py
@@ -6,4 +6,4 @@ class Language(models.Model):
     name = models.CharField(max_length=255)
     year = models.CharField(max_length=255)
     developer_company = models.CharField(max_length=255, blank=True, null=True)
-    precessors = models.CharField(max_length=255, blank=True, null=True)
+    predescessors = models.CharField(max_length=255, blank=True, null=True)
