17d2cea9c88fb6992618eb18f30795514a0e1184
Daniel Corcoran <danielc.92@outlook.com>
2019-06-17 13:40:18 +1000

    Added view contents from viewsets

--- languages/views.py
+++ languages/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .modesl import Language
+from .serializer import LanguageSerializer
 
 # Create your views here.
+class LanguageView(viewsets.ModelViewSet):
+    
+    queryset = Language.objects.all()
+    serializer_class = LanguageSerializer
