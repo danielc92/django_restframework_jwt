73fb7daba6c47ec9f655af5c2c87e2fc4930dd18
Daniel Corcoran <danielc.92@outlook.com>
2019-06-17 13:41:40 +1000

    Added urls paths with built in routers

--- languages/urls.py
+++ languages/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path, include
+from rest_framework import routers
+from . import views
 
+router = routers.DefaultRouter()
+router.register('languages', views.LanguageView)
 
 urlpatterns = [
-
+    path('', include(router.urls))
 ]
