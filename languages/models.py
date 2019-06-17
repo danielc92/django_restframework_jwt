529e1cc4bb206ca76536c03b807f2e525c71ad70
Daniel Corcoran <danielc.92@outlook.com>
2019-06-17 13:17:16 +1000

    Added model

--- languages/models.py
+++ languages/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Language(models.Model):
+    name = models.CharField(max_length=255)
+    year = models.CharField(max_length=255)
+    developer_company = models.CharField(max_length=255, blank=True, null=True)
+    precessors = models.CharField(max_length=255, blank=True, null=True)
